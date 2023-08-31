5845ec0c6570a7f0b3a00fc10f2acdd5b76fa10e
Matheus Cardoso <matheuspcardoso@gmail.com>
2023-08-31 14:58:42 -0300

    remover parte da dependencia descontinuada

--- env.ts
+++ env.ts
@@ -1,11 +1,5 @@
-// eslint-disable-next-line import/no-named-as-default
-import Constants from 'expo-constants';
-
 function getApiUrl() {
-  const baseUrl = Constants.expoConfig?.extra?.API_URL;
-  if (!baseUrl) return 'http://18.222.105.195:8080/';
-
-  return baseUrl;
+  return 'http://18.222.105.195:8080/';
 }
 
 export const Env = {
